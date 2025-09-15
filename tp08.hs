72a9e9967da6f72f31ca2e2ecb66d20962b704f4
zkolsh <129521109+zkolsh@users.noreply.github.com>
2025-09-15 20:24:19 +0000

    fucking comentarios de mierda

--- tp08.hs
+++ tp08.hs
@@ -5,6 +5,7 @@ import qualified Data.Map as M
 import           Data.Map           (Map, (!))
 import           System.Environment (getArgs)
 
+-- Interface de cola de prioridad.
 class PriorityQueue pq where
         pqempty :: pq a
         pqisEmpty :: pq a -> Bool
@@ -15,6 +16,9 @@ class PriorityQueue pq where
 extractPQueue :: (PriorityQueue pq, Ord a) => pq a -> (a, pq a)
 extractPQueue pque = (pqfront pque, pqdequeue pque)
 
+-- Heap para tener una instancia eficiente de cola de prioridad.
+-- Se mantiene el tamaño del heap como miembro para no perder tiempo volviendo a
+-- contar cuántos nodos hay en total.
 data Heap a = HEmpty | HNode !a !Int !(Heap a) !(Heap a) deriving (Show, Eq)
 
 heapFrom :: a -> Heap a
@@ -28,13 +32,14 @@ instance PriorityQueue Heap where
         pqfront = minHeap
         pqdequeue = deleteHeap
 
+-- Implementaciones heap; del apunte.
 minHeap :: Heap a -> a
 minHeap HEmpty = undefined
 minHeap (HNode x _ _ _) = x
 
 hsize :: Heap a -> Int
 hsize HEmpty = 0
-hsize (HNode _ s _ _) = s
+hsize (HNode _ !s _ _) = s
 
 insertHeap :: Ord a => a -> Heap a -> Heap a
 insertHeap x HEmpty = HNode x 1 HEmpty HEmpty
@@ -74,20 +79,31 @@ listasAGrilla n m lista = M.fromList [((x, y), val)
 costosMinimos :: Int -> Int -> Int -> Map (Int, Int) Int -> [Int]
 costosMinimos n m k grilla = reverse (dijkstra grilla k n m)
 
+-- Representa un camino que estamos explorando actualmente.
+-- Estamos en la posición (x, y) con el costo acumulado c.
 data Paso = Paso (Int, Int) Int deriving (Eq, Show)
 
 instance Ord Paso where
         (Paso _ c1) <= (Paso _ c2) = c1 <= c2
 
+-- Usamos el algoritmo de Dijkstra para encontrar los K caminos minimos.
+-- El algoritmo en sí no es muy distinto de un recorrido iterativo de un grafo.
+-- Cuando el grafo tiene costos, si lo exploramos con una cola de prioridad, y vamos
+-- acumulando el costo de recorrer cada nodo, el resultado es que exploramos el
+-- grafo por los caminos más cortos.
+-- Las primeras K veces que lleguemos a la esquina inferior-derecha, está garantizado
+-- entonces que habrán sido los K caminos mínimos.
 dijkstra :: Map (Int, Int) Int -> Int -> Int -> Int -> [Int]
 dijkstra grilla kinicial m n = go kinicial [] visitasInicial heapInicial
         where !heapInicial = heapFrom (Paso (0, 0) (grilla ! (0, 0)))
               !visitasInicial = listasAGrilla n m (replicate n (replicate m 0))
               go 0 acc _ _ = acc
               go !k acc !visitas !pq
+                -- Casos base:
                 | pqisEmpty pq = acc
                 | x >= n || y >= m = go k acc visitas pq'
                 | x == (n - 1) && y == (m - 1) = go (k - 1) (c : acc) visitas' pq'
+                -- Casos recursivos:
                 | visitas ! (x, y) >= k = go k acc visitas pq'
                 | otherwise = go k acc visitas' (pqenqueue abajo (pqenqueue derecha pq'))
                where (p, pq') = extractPQueue pq
@@ -96,6 +112,7 @@ dijkstra grilla kinicial m n = go kinicial [] visitasInicial heapInicial
                      abajo   = Paso (x, y + 1) (c + M.findWithDefault 0 (x, y + 1) grilla)
                      derecha = Paso (x + 1, y) (c + M.findWithDefault 0 (x + 1, y) grilla)
 
+-- Para poder probar el programa desde GHCi.
 run :: FilePath -> IO ()
 run archivo = do
   (n, m, k, listas) <- leerArchivo archivo
